daca78d9f065de7418b33e55f3c07142eb5931ca
zurgeg <46549042+zurgeg@users.noreply.github.com>
2023-01-23 15:36:47 +0000

    move doc stuff

--- types.go
+++ types.go
@@ -8,8 +8,10 @@ type MovieClip struct {
 	Children []any
 }
 
-func (this MovieClip) CreateEmptyMovieClip (instanceName string, depth int) *any {
-	// Creates an empty movie clip as a child of the current clip. Returns pointer to created clip.
+// Creates an empty movie clip as a child of the current clip. Returns pointer to created clip.
+// P.S. Yes, I know, you aren't supposed to use "this" as a reciever, but I'm trying to remain
+// as close to the original flash version as possible
+func (this MovieClip) CreateEmptyMovieClip(instanceName string, depth int) *any {
 	this.Children = append(this.Children, MovieClip{[]any{}})
-	return &this.Children[len(this.Children) - 1]
+	return &this.Children[len(this.Children)-1]
 }
