338b970ce156194424035b8304dbc868a6998c86
zurgeg <46549042+zurgeg@users.noreply.github.com>
2023-01-23 20:15:30 -0500

    update types

--- types.go
+++ types.go
@@ -8,18 +8,30 @@ type MovieClipIface interface {
 
 // MovieClip struct, do not create this yourself, instead use goclips.Root.CreateEmptyMovieClip
 type MovieClip struct {
-	Children []any
+	Children     []any
+	InstanceName string
+	Depth        int
+	currentFrame int
 }
 
 // Creates an empty movie clip as a child of the current clip. Returns pointer to created clip.
 // P.S. Yes, I know, you aren't supposed to use "this" as a reciever, but I'm trying to remain
 // as close to the original flash version as possible
 func (this MovieClip) CreateEmptyMovieClip(instanceName string, depth int) *any {
-	this.Children = append(this.Children, MovieClip{[]any{}})
+	this.Children = append(this.Children, MovieClip{
+		Children:     []any{},
+		InstanceName: instanceName,
+		Depth:        depth,
+	})
 	return &this.Children[len(this.Children)-1]
 }
 
 // Gets a child clip
 func (this MovieClip) GetChild(childIndex int) *any {
 	return &this.Children[childIndex]
+}
+
+func (this MovieClip) NextFrame() {
+	// at some point actually display this frame
+	this.currentFrame++
 }
